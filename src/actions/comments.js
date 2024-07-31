8a501ec2d9e3e859da9ae86d8aa4919435dca36c
Theo Reeves <theo_25@icloud.com>
2024-07-31 10:20:44 +0100

    added database stuff

--- src/actions/comments.js
+++ src/actions/comments.js
@@ -4,6 +4,7 @@ import { auth } from "@/auth";
 import { db } from "@/db";
 import { revalidatePath } from "next/cache";
 
+//random change
 export async function saveComment({ postId, parentCommentId }, formData) {
   const session = await auth();
 
